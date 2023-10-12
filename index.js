a9d85ada360d211e4f3bdf36d6141868439d0b3c
Dhanush MC <dhanushmc93@gmail.com>
2023-10-12 17:17:55 +0530

    commit

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const mongoDB = require("../backend/db");
 mongoDB();
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://jazzy-piroshki-fa94ee.netlify.app/"
+  );
   res.header(
     "Access-Control-Allow-Headers",
     "Origin,X-Requested-With,Content-Type,Accept"
